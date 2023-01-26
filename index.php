2289c8cdd8e35803184570884a5b4f360882e5c5
LorenzoOgnibene <lorenzo.ognibene@gmail.com>
2023-01-26 15:06:55 +0100

    Password generator

--- index.php
+++ index.php
@@ -7,6 +7,22 @@
         <title>Strong Password Generator</title>
     </head>
     <body>
+        <form action="index.php" method="get">
+        <label for="length">Inserisci la lunghezza della password (min 8 caratteri):</label>
+        <input type="number" id="length" name="length" min="8" max="32">
+        <input type="submit">
+    </form>
 
+    <?php
+        if(isset($_GET['length'])) {
+            $length = $_GET['length'];
+            $characters = array_merge(range('a','z'), range('A','Z'), range(0,9), array('!','@','#','$','%','^','&','*'));
+            $password = "";
+            for($i = 0; $i < $length; $i++) {
+                $password .= $characters[array_rand($characters)];
+            }
+            echo "La tua password: " . $password;
+        }
+    ?>
     </body>
 </html>
